9516c46e1b90a866d0b35fda7c52aaf38138e1c4
persadian <34051185+arishma108@users.noreply.github.com>
2023-10-06 18:41:05 +0200

    Create CalculateKineticEnergy.py

---
+++ Program's_Contributed_By_Contributors/Python_Programs/CalculateKineticEnergy.py
@@ -0,0 +1,29 @@
+print("This program allows persadian to calculate the kinetic energy of a moving object.")
+
+mass = float(input("Enter the object's mass in kilograms: "))
+speed = float(input("Enter the object's speed in meters per second: "))
+
+kinetic_energy = 0.5 * mass * speed ** 2
+
+print("The object has", kinetic_energy, "joules of energy.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
